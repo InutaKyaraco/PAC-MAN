344657ad145c965c9a78709d8c36fb1ad1f4b28f
inuta <41900456+InutaKyaraco@users.noreply.github.com>
2019-07-10 15:55:27 +0900

    Create Item
    New Item Create

---
+++ PAC-MAN/Assets/ItemController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player") {
+
+            if (this.gameObject.tag == "PowerCookie") {
+                Debug.Log("Power");
+                //プレイヤー強化＆敵逃げる　コルーチン使えば多分おけ
+            }
+
+            if (this.gameObject.tag == "Cookie") {
+                //得点アップ。UIにお知らせする
+                Debug.Log("Cookie");
+            }
+            Destroy(this.gameObject);
+        }
+    }
+}
--- PAC-MAN/Assets/PlayerController.cs
+++ PAC-MAN/Assets/PlayerController.cs
@@ -4,19 +4,104 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Vector3 touchStartPos;
+    Vector3 touchEndPos;
+    enum Directions {
+        Right = 0, Left = 1, Up = 2, Down = 3
+    };
+    int PlayerState;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerState = (int)Directions.Left;
     }
 
     // Update is called once per frame
     void Update()
     {
+        DetectFric();
+        MovePlayer();
+    }
+
+    void DetectFric()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            this.touchStartPos = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0)) {
+            this.touchEndPos = Input.mousePosition;
+        }
+
+        GetDirection();
+    }
+
+    void GetDirection()
+    {
+        float directionX = touchEndPos.x - touchStartPos.x;
+        float directionY = touchEndPos.y - touchStartPos.y;
+
+        if (Mathf.Abs(directionX) > Mathf.Abs(directionY)) {
+            if (directionX > 30) {
+                //Right Fric
+                PlayerState = (int)Directions.Right;
+                Debug.Log("Right");
+            }
+            else if (directionY < -30) {
+                //Left Fric
+                PlayerState = (int)Directions.Left;
+                Debug.Log("Left");
+            }
+        }
+        else if(Mathf.Abs(directionY) > Mathf.Abs(directionX)){
+            if (directionY > 30) {
+                //Up Fric
+                PlayerState = (int)Directions.Up;
+                Debug.Log("Up");
+            }
+            else if (directionY < -30) {
+                //Down Fric
+                PlayerState = (int)Directions.Down;
+                Debug.Log("Down");
+            }
+        }
+    }
+
+
+    void MovePlayer() {
+        switch (PlayerState) {
+            case (int)Directions.Right:
+                MoveRight();
+                break;
+
+            case (int)Directions.Left:
+                MoveLeft();
+                break;
+
+            case (int)Directions.Up:
+                MoveUp();
+                break;
+
+            case (int)Directions.Down:
+                MoveDown();
+                break;
+        }
+    }
+
+    void MoveRight() {
+        this.transform.Translate(0.1f, 0, 0, Space.World);
         
     }
 
-    void Fric() {
+    void MoveLeft() {
+        this.transform.Translate(-0.1f, 0, 0, Space.World);
+    }
 
+    void MoveUp() {
+        this.transform.Translate(0, 0, 0.1f, Space.World);
+    }
+
+    void MoveDown() {
+        this.transform.Translate(0, 0, -0.1f, Space.World);
     }
 }
